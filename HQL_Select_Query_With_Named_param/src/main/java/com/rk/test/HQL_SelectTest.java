9574003683ff469f4d8c8df83ef63639469f5994
Rakesh <rakesh.subudhi50@gmail.com>
2020-09-27 14:50:03 +0530

    @named

---
+++ HQL_Select_Query_With_Named_param/src/main/java/com/rk/test/HQL_SelectTest.java
@@ -0,0 +1,56 @@
+package com.rk.test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.rk.entity.InsurancePolicy;
+import com.rk.utility.HibernateUtil;
+
+public class HQL_SelectTest {
+
+	public static void main(String[] args) {
+		Session ses=null;
+		Transaction tx=null;
+		Query<InsurancePolicy> query=null;
+		List<InsurancePolicy> list=null;
+		
+	     //get Session obj
+		ses=HibernateUtil.getSession();
+		try {
+			tx=ses.beginTransaction(); //dummy
+		
+			//prepare Query
+			query=ses.createQuery("FROM  com.rk.entity.InsurancePolicy where policy_id IN(: org1,:org2)");
+			
+			//set query param values
+			
+			/*  query.setInteger(1,100);
+			      query.setInteger(2, 105);   *///this method are deprecated
+			
+			//use
+			query.setParameter("org1", 100);
+			query.setParameter("org2",101);
+			
+			//Execute the query 
+			list=query.getResultList();
+			
+			//print the output
+			list.forEach(System.out::println);
+			
+		}//try
+		
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		
+		finally {
+			HibernateUtil.closeSessionFacory();
+		}
+
+	}//main
+}//class
