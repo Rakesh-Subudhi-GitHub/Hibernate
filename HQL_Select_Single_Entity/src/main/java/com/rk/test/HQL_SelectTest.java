c2fef4f0990bcfa065864ce2a8d86f3c6fafa2ae
Rakesh <rakesh.subudhi50@gmail.com>
2020-09-28 14:38:10 +0530

    single_entity

---
+++ HQL_Select_Single_Entity/src/main/java/com/rk/test/HQL_SelectTest.java
@@ -0,0 +1,60 @@
+package com.rk.test;
+
+import java.util.List;
+import java.util.Iterator;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.rk.entity.InsurancePolicy;
+import com.rk.utility.HibernateUtil;
+
+public class HQL_SelectTest {
+
+	public static void main(String[] args) {
+		Session ses=null;
+		Transaction tx=null;
+		//Query<InsurancePolicy> query=null;
+		//List<InsurancePolicy> list=null;
+		
+	     //get Session obj
+		ses=HibernateUtil.getSession();
+		try {
+			tx=ses.beginTransaction(); //dummy
+		
+			//prepare Query
+			Query query=ses.createQuery("FROM   com.rk.entity.InsurancePolicy WHERE policy_id= :pid");
+			
+			//set values to query params
+			query.setParameter("pid",200);
+			
+			//execute HQL query
+			List<InsurancePolicy> list=query.getResultList();
+			
+			//process result
+			if(!list.isEmpty())
+			{
+		    
+				InsurancePolicy policy=list.get(0);//single record so fast element in array so its start with 0 so we get 0th elements
+		        System.out.println(policy);
+		        System.out.println("size of elements:: "+list.size());
+			
+			}//if
+			else
+			{
+				System.out.println("record not found:::");
+			}
+		
+	  }		//try
+		
+		catch(HibernateException he) {
+			he.printStackTrace();
+		}
+		
+		finally {
+			HibernateUtil.closeSessionFacory();
+		}
+
+	}//main
+}//class
