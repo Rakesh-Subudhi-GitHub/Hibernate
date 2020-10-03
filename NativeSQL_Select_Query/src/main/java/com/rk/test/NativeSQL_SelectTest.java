1fad6c6f788cccc2991e4f2df3da5112a5de7e3d
Rakesh <rakesh.subudhi50@gmail.com>
2020-10-03 12:08:29 +0530

    native Select query

--- NativeSQL_Select_Query/src/main/java/com/rk/test/NativeSQL_SelectTest.java
+++ NativeSQL_Select_Query/src/main/java/com/rk/test/NativeSQL_SelectTest.java
@@ -3,6 +3,7 @@ package com.rk.test;
 import java.util.List;
 
 import org.hibernate.HibernateException;
+import org.hibernate.query.NativeQuery;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -10,36 +11,37 @@ import org.hibernate.query.Query;
 import com.rk.entity.InsurancePolicy;
 import com.rk.utility.HibernateUtil;
 
-public class HQL_SelectTest {
+public class NativeSQL_SelectTest {
 
 	public static void main(String[] args) {
 		Session ses=null;
 		Transaction tx=null;
-		Query<InsurancePolicy> query=null;
-		List<InsurancePolicy> list=null;
 		
 	     //get Session obj
 		ses=HibernateUtil.getSession();
+		
 		try {
+			if(!ses.getTransaction().isActive())
 			tx=ses.beginTransaction(); //dummy
 		
 			//prepare Query
-			query=ses.createQuery("FROM  com.rk.entity.InsurancePolicy where policy_id >=?1 and policy_id<=?2");
+			NativeQuery query=ses.createSQLQuery("SELECT * FROM Insurance_Policy where policy_id >=? and policy_id<=?");
 			
 			//set query param values
-			
-			/*  query.setInteger(1,100);
-			      query.setInteger(2, 105);   *///this method are deprecated
-			
-			//use
 			query.setParameter(1, 100);
 			query.setParameter(2,105);
 			
 			//Execute the query 
-			list=query.getResultList();
+			List<Object[]> list=query.getResultList();
 			
 			//print the output
-			list.forEach(System.out::println);
+			list.forEach(row->{
+				for(Object o:row)
+				{
+					System.out.print(o+" ");
+				}
+				System.out.println();
+			});
 			
 		}//try
 		
